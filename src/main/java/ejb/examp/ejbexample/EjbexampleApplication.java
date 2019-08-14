8b3cc1ce0dbf0911193b013fbe52f2cfac2d274e
NamDV <35649331+NamDVD00453@users.noreply.github.com>
2019-08-14 11:35:29 +0700

    Initial commit

---
+++ src/main/java/ejb/examp/ejbexample/EjbexampleApplication.java
@@ -0,0 +1,13 @@
+package ejb.examp.ejbexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjbexampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EjbexampleApplication.class, args);
+    }
+
+}
